6fd02368c18489009a0c77e01366dd560ff835a9
nirvanadantas <nirvanadesara@gmail.com>
2014-12-10 13:10:01 -0300

    rele pelo serial

--- releteste/releteste.ino
+++ releteste/releteste.ino
@@ -2,18 +2,22 @@
 /*####################   RELE   ####################*/
 
 int pinorele = 2;
-
+char c;
 
 void setup() {
   pinMode(pinorele, OUTPUT);
+  Serial.begin(9600);
+  digitalWrite(pinorele, LOW);
 }
 
 void loop() {
-  digitalWrite(pinorele,  HIGH);
-  delay(5000);
+  while(Serial.available() > 0){
+    c = Serial.read();
+    if (c=='1')  digitalWrite(pinorele,  HIGH);
   
-  digitalWrite(pinorele,  LOW);
-  delay(5000);
+    if(c=='0')  digitalWrite(pinorele,  LOW);
+    Serial.println(pinorele);
+  }
   
 
 }
